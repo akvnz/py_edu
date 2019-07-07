d3cdd7386744a0095e3ef93b954ac18bf265683c
akvnz <42027504+akvnz@users.noreply.github.com>
2019-07-07 23:22:22 +0300

    implemented short url service

--- short_url_creator/app/models.py
+++ short_url_creator/app/models.py
@@ -1,21 +1,13 @@
 from datetime import datetime
 from app import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-class Post(db.Model):
+class Link(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
+    original_url = db.Column(db.String(140))
+    short_url = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Original {}, short {}>'.format(self.original_url, self.short_url)
+
--- short_url_creator/app/routes.py
+++ short_url_creator/app/routes.py
@@ -1,26 +1,38 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, redirect, url_for, request
+from app import app, db
+from app.forms import ShortenerForm
+from app.models import Link
+import short_url
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Guest'}
-    posts = [
-        {
-            'author': {'username': 'Guest'},
-            'body': 'Latest news'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    original_url = request.args.get('original_url')
+    link = Link(original_url=original_url)
+    db.session.add(link)
+    db.session.commit()
+    param = short_url.encode_url(link.id)
+    shortened_url = "{}{}".format(
+        request.url_root,
+        param
+    )
+
+    link.short_url = shortened_url
+    db.session.commit()
+
+    return render_template('result.html',  title='Result', short_url=shortened_url)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+@app.route('/', defaults={'shorl_url_code': None}, methods=['GET', 'POST'])
+@app.route('/<shorl_url_code>', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index(shorl_url_code=None):
+    if shorl_url_code is not None:
+        link = Link.query.get(str(short_url.decode_url(shorl_url_code)))
+        if link is not None:
+            return redirect(link.original_url)
+
+    form = ShortenerForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
+        return redirect(url_for('result', original_url=form.original_url.data))
+    return render_template('index.html',  title='Shortener', form=form)
