651cfd02afef934e971ddd4339db71e21edae431
akvnz <42027504+akvnz@users.noreply.github.com>
2019-07-11 23:41:44 +0300

    edited .gitignore

--- short_url_creator/app/__init__.py
+++ short_url_creator/app/__init__.py
@@ -2,10 +2,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_redis import FlaskRedis
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+app.config['REDIS_HOST'] = 'localhost'
+app.config['REDIS_PORT'] = 6379
+app.config['REDIS_DB'] = 0
+redis = FlaskRedis(app)
 
 from app import routes, models
--- short_url_creator/app/routes.py
+++ short_url_creator/app/routes.py
@@ -1,36 +1,29 @@
 from flask import render_template, redirect, url_for, request
-from app import app, db
+from app import app, db, redis
 from app.forms import ShortenerForm
-from app.models import Link
-import short_url
+import uuid
 
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
     original_url = request.args.get('original_url')
-    link = Link(original_url=original_url)
-    db.session.add(link)
-    db.session.commit()
-    param = short_url.encode_url(link.id)
-    shortened_url = "{}{}".format(
-        request.url_root,
-        param
-    )
-
-    link.short_url = shortened_url
-    db.session.commit()
+    short_url_code = uuid.uuid4().hex
+    shortened_url = "{}{}".format(request.url_root, short_url_code)
+    redis.set(short_url_code, original_url, ex=2592000)
 
     return render_template('result.html',  title='Result', short_url=shortened_url)
 
 
 @app.route('/', defaults={'shorl_url_code': None}, methods=['GET', 'POST'])
-@app.route('/<shorl_url_code>', methods=['GET', 'POST'])
+@app.route('/<short_url_code>', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-def index(shorl_url_code=None):
-    if shorl_url_code is not None:
-        link = Link.query.get(str(short_url.decode_url(shorl_url_code)))
+def index(short_url_code=None):
+    if short_url_code is not None \
+            and short_url_code != 'index'\
+            and short_url_code != 'favicon.ico':
+        link = redis.get(short_url_code)
         if link is not None:
-            return redirect(link.original_url)
+            return redirect(link)
 
     form = ShortenerForm()
     if form.validate_on_submit():
